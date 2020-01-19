6d66edf567e1f7ea041686ab2059a7b06931ae19
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-01-19 21:50:32 +0100

    Add single post view

--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,13 @@ var upload = multer({ dest: './public/images' });
 var mongodb = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
 
+router.get('/show/:id', function(req, res, next) {
+  var posts = db.get('posts');
+  posts.findById(req.params.id, function(err, post) {
+    res.render('show', { post: post });
+  });
+});
+
 router.get('/add', function(req, res, next) {
   var categories = db.get('categories');
   var authors = db.get('authors');
